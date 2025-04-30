b38b09b84390963278703b7bdc1c2289afb9002c
Miles Georgi <azimux@gmail.com>
2025-04-30 11:03:18 -0700

    Add a production crud driver

--- config/initializers/crud.rb
+++ config/initializers/crud.rb
@@ -1,12 +1,20 @@
 require "foobara/local_files_crud_driver"
 
-Foobara::Persistence.default_crud_driver = if Rails.env.development?
+Foobara::Persistence.default_crud_driver = case Rails.env
+                                           when "development"
                                              # :nocov:
                                              Foobara::LocalFilesCrudDriver.new
                                              # :nocov:
-                                           elsif Rails.env.test?
+                                           when "test"
                                              Foobara::Persistence::CrudDrivers::InMemory.new
+                                             # rubocop:disable Lint/DuplicateBranch
+                                           when "production"
+                                             # TODO: change this to Redis crud driver
+                                             # :nocov:
+                                             Foobara::Persistence::CrudDrivers::InMemory.new
+                                             # :nocov:
                                            else
+                                             # rubocop:enable Lint/DuplicateBranch
                                              # :nocov:
                                              raise "Unknown environment: #{Rails.env}"
                                              # :nocov:
