9c6e4d16657a503a28aca9d0ae18c6a268cc0147
Miles Georgi <azimux@gmail.com>
2025-05-05 14:00:06 -0700

    Support multiple CORS_ORIGIN values

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("CORS_ORIGIN", "http://localhost:3001")
+    origins ENV.fetch("CORS_ORIGIN", "http://localhost:3001").split(";").map(&:strip)
     resource "*",
              headers: :any,
              expose: ["X-Access-Token"],
